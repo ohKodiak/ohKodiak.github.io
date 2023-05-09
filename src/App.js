19caca2bc5bc08ee5e617207a4cd9e7f727cfa5a
e <77172196+ohKodiak@users.noreply.github.com>
2023-05-09 01:45:52 -0500

    more

--- src/App.js
+++ src/App.js
@@ -9,19 +9,38 @@ class App extends Component {
           <h1 className="App-title">howdy, glad to have you here</h1>
         </header>
         <p className="App-intro">
-          my name is ezra, and I am a computer engineer.
+          my name is ezra, and i am a computer engineer.
         </p>
         <div className="App-images">
           <img className="App-image animated fadeIn" src="https://www.australiangeographic.com.au/wp-content/uploads/2018/06/kodiak-bear-768x452.jpg" alt="placeholder image" />
         </div>
         <p>
-          I have experience in [List Your Skills or Projects]. I am passionate about [Your Interests or Hobbies].
+          i like to code. i like animals. i like texas a&m. i like the aggie coding club and the texas a&m cyber security club, both of which i am fortunate enough to serve as an officer for. 
+          i like making things that people value. 
         </p>
+        
+        
+        <h2>more of me</h2>
+        <ul>
+          <li>email: [ezra@tamu.edu]</li>
+          <li>linkedin: [my name]</li>
+          <li>github: [github/ohKodiak]</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*
         <h2>Education</h2>
         <ul>
           <li>[Your Degree], [Your Major], [Your University], [Graduation Year]</li>
           <li>[Your Degree], [Your Major], [Your University], [Graduation Year]</li>
         </ul>
+        
         <h2>Experience</h2>
         <h3>[Your Job Title], [Your Company]</h3>
         <p>[Brief Description of Your Role and Accomplishments]</p>
@@ -37,10 +56,4 @@ class App extends Component {
           <li>Email: [Your Email Address]</li>
           <li>LinkedIn: [Link to Your LinkedIn Profile]</li>
           <li>GitHub: [Link to Your GitHub Profile]</li>
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default App;
+        </ul> */
