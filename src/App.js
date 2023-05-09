b430943444f40dd725d16ff41875be2b6cb0a6ff
e <77172196+ohKodiak@users.noreply.github.com>
2023-05-09 00:11:01 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to My Personal Website</h1>
+        </header>
+        <p className="App-intro">
+          My name is [Your Name], and I am a [Your Profession/Title].
+        </p>
+        <div className="App-images">
+          <img className="App-image animated fadeIn" src="https://wallpapercave.com/wp/wp7955438.png" alt="placeholder image" />
+        </div>
+        <p>
+          I have experience in [List Your Skills or Projects]. I am passionate about [Your Interests or Hobbies].
+        </p>
+        <h2>Education</h2>
+        <ul>
+          <li>[Your Degree], [Your Major], [Your University], [Graduation Year]</li>
+          <li>[Your Degree], [Your Major], [Your University], [Graduation Year]</li>
+        </ul>
+        <h2>Experience</h2>
+        <h3>[Your Job Title], [Your Company]</h3>
+        <p>[Brief Description of Your Role and Accomplishments]</p>
+        <h3>[Your Job Title], [Your Company]</h3>
+        <p>[Brief Description of Your Role and Accomplishments]</p>
+        <h2>Projects</h2>
+        <ul>
+          <li>[Project Name], [Brief Description], [Link to Project Website/GitHub Repository]</li>
+          <li>[Project Name], [Brief Description], [Link to Project Website/GitHub Repository]</li>
+        </ul>
+        <h2>Contact Me</h2>
+        <ul>
+          <li>Email: [Your Email Address]</li>
+          <li>LinkedIn: [Link to Your LinkedIn Profile]</li>
+          <li>GitHub: [Link to Your GitHub Profile]</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
