9a31b54cbde5531608d8019b91dd885136c740da
e <77172196+ohKodiak@users.noreply.github.com>
2023-09-01 19:25:28 -0500

    added more new page output

--- src/pages/NewPage.js
+++ src/pages/NewPage.js
@@ -18,13 +18,13 @@ function EvenMore() {
     const projectsRegex = /^(?!.*\b(not|don't|do not|no)\b)(?=.*\b(projects|project)\b)/i;
   
     if (skillsRegex.test(inputValue)) {
-      setOutput("I have expertise in various programming languages and frameworks.");
+      setOutput("I consider myself foremost a problem solver, and programing and engineering are skills that I can use to solve problems. More specificially, I have basic profiency in Python, C++, javaScript, Typescript, Verilog, SQL, and MATLAB. I work well, if not best, in teams and collaborative enviroments but I can meet deadlines individually as well.  ");
     } else if (experienceRegex.test(inputValue)) {
-      setOutput("I have worked on several projects in the past, gaining valuable experience.");
+      setOutput("I have worked as a software intern, software developer, and a hardware security research assistant. I am also the events and marketing chair at the Aggie Coding Club and on the tech commitee at the Cyber Security Club.");
     } else if (projectsRegex.test(inputValue)) {
-      setOutput("I have developed several projects, including web applications and mobile apps.");
+      setOutput("I have worked on several projects in the past, gaining valuable experience. I worked with a team to create a file cabinet note with a unique UI that imitated the sliding motion of pulling files with data on them from filing cabinets. I also have a project called Browser Extensions where I worked with a team to create a browser extension to enhance the functionality of our class homepage canvas, remaking the UI to make it more relaxing and visually pleasing with a dark mode and embedded lofi youtube videos. I also have experience using Blender to model a church to scale for a research group at Texas A&M. ");
     } else {
-      setOutput("I'm sorry, I don't have an answer for that input.");
+      setOutput("I'm sorry, I don't have an answer for that input. Please indicate whether you would like to hear about either my skills, experience, or projects. ");
     }
   };
   
